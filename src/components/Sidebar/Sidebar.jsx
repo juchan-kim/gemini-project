e5004714c624b5334fe8bbba1a2da6843ee7a4dc
juchan-kim <113649125+juchan-kim@users.noreply.github.com>
2025-01-06 13:38:06 +0900

    gemini asnwer on webpage is working

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import './Sidebar.css'
+import { assets } from "../../assets/assets"
+import { useState } from 'react'
+
+const Sidebar = () => {
+  const [extended, setExtended] = useState(false)
+  
+  return (
+      <div className='sidebar' >
+        <div className="top">
+          <img className='menu' onClick={() => setExtended(prev => !prev)} src={assets.menu_icon} alt="" />
+          {extended ? 
+          <div className="new-chat">
+            <img src={assets.plus_icon} alt="" />
+            <p>New Chat</p>
+          </div> :
+          <div className="new-chat">
+            <img src={assets.plus_icon} alt="" />
+          </div>
+          }
+        {extended ?  
+        <div className="recent">
+          <p className="recent-title">Recent</p>
+          <div className="recent-entry">
+            <img src={assets.message_icon} alt="" />
+            <p>What is react ...</p>
+          </div>
+        </div> :
+        null
+        }
+        </div>
+        <div className="bottom" >
+        <div className="bottom-item recent-entry">
+        <img src={assets.question_icon} alt="" />
+        {extended ?
+        <p>Help</p> :
+        null  
+        }
+        </div>
+        <div className="bottom-item recent-entry">
+        <img src={assets.history_icon} alt="" />
+        {extended ?
+        <p>Activity</p> :
+        null
+        }
+        </div>
+        <div className="bottom-item recent-entry">
+        <img src={assets.setting_icon} alt="" />
+        {extended ?
+        <p>Settings</p> :
+        null
+        }
+        </div>
+        </div>
+      </div>
+  )
+}
+
+export default Sidebar
